11de995fd1cbff7f962a4b87359373001486e553
Joseph <141687242+JHsin23@users.noreply.github.com>
2023-11-02 21:49:40 -0400

    Create Market.java
    Framework for the Market main function

---
+++ Market.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+//MOST METHODS AND OBJECT CALLS HAVE NOT BE CREATED
+
+public class Market {
+    
+    public static void main(String[] args) {
+
+        //accoubt creation and login
+        if (account.isSeller()) {
+
+        } else {
+
+
+        }
+
+        System.out.println("Welcome to the Marketplace!");
+        System.out.println("Available products");
+        System.out.println("-------------------");
+
+        for (int i = 0; i < productsArrayList.length(); i++) {
+            int j = i+1;
+            System.out.println(j + productsArrayList.get(i).listingPage());
+            System.out.println("-------------------");
+
+        }
+
+
+        boolean exit = false;
+        while (!exit) {
+
+            System.out.println("Select the product number");
+
+            Scanner scan = new Scanner(System.in);
+            int selection = scan.nextInt();
+            product selectedProduct =  productsArrayList.get(selection - 1);
+
+            System.out.println(selectedProduct.toString());
+
+            System.out.println("Do you want to purchase this? 0 for yes, 1 for no");
+            int purchaseMaybe = scan.nextInt();
+
+
+        }
+
+
+
+    }
+
+
+}
