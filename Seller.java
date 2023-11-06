ad56d1ebb9a5226a19ca172465d6c0bc972f4b48
AnnaliseW <130863140+AnnaliseW@users.noreply.github.com>
2023-11-05 19:26:49 -0500

    Update Seller.java

--- Seller.java
+++ Seller.java
@@ -1,8 +1,13 @@
+
 import java.util.ArrayList;
 
-public class Seller extends Product {
-    private double revenue;
-    private ArrayList<String> sales;
+public class Seller extends User {
+    private static double revenue;
+    private static ArrayList<String> sales;
+    public static ArrayList<Product> itemsSellingBySeller;
+
+    public static ArrayList<Product> itemsSold;
+    // ?? similar to sales maybe delete
     // sales could include... products sold, customer name(occurs when bought added), revenue added
 
 
@@ -13,6 +18,22 @@ public class Seller extends Product {
         super(productName, storeName, descriptionOfProduct, quantityAvailable, price);
         this.revenue = revenue;
         this.sales = sales;
+    }
+
+    public static ArrayList<Product> getItemsSellingBySeller() {
+        return itemsSellingBySeller;
+    }
+
+    public static void setItemsSellingBySeller(ArrayList<Product> itemsSellingBySeller) {
+        Seller.itemsSellingBySeller = itemsSellingBySeller;
+    }
+
+    public static ArrayList<Product> getItemsSold() {
+        return itemsSold;
+    }
+
+    public static void setItemsSold(ArrayList<Product> itemsSold) {
+        Seller.itemsSold = itemsSold;
     }
 
     public double getRevenue() {
@@ -31,34 +52,39 @@ public class Seller extends Product {
         this.sales = sales;
     }
 
-    public void addProduct(Product addedProduct) {
-        getProductsArrayList().add(addedProduct);
+    public static void addProduct(Product addedProduct) {
+        Product.getProductsArrayList().add(addedProduct);
     }
 
-    public void removeProduct(Product product) {
-        for (int i = 0; i < getProductsArrayList().size(); i++) {
-            if (getProductsArrayList().get(i).equals(product)) {
-                getProductsArrayList().remove(i);
+    public static void removeProduct(Product product) {
+        for (int i = 0; i < Product.getProductsArrayList().size(); i++) {
+            if (Product.getProductsArrayList().get(i).equals(product)) {
+                Product.getProductsArrayList().remove(i);
             }
         }
     }
 
-    public void modifyProduct(Product product, String newProductName, String newStoreName,
+    public static void modifyProduct(Product product, String newProductName, String newStoreName,
                               String newDescriptionOfProduct, int newQuantityAvailable, double newPrice) {
-        for (int i =0 ; i < getProductsArrayList().size(); i++) {
-            if (getProductsArrayList().get(i).equals(product)) {
-                getProductsArrayList().get(i).setProductName(newProductName);
-                getProductsArrayList().get(i).setStoreName(newStoreName);
-                getProductsArrayList().get(i).setDescriptionOfProduct(newDescriptionOfProduct);
-                getProductsArrayList().get(i).setQuantityAvailable(newQuantityAvailable);
-                getProductsArrayList().get(i).setPrice(newPrice);
+        for (int i = 0; i < Product.getProductsArrayList().size(); i++) {
+            if (Product.getProductsArrayList().get(i).equals(product)) {
+                Product.getProductsArrayList().get(i).setProductName(newProductName);
+                Product.getProductsArrayList().get(i).setStoreName(newStoreName);
+                Product.getProductsArrayList().get(i).setDescriptionOfProduct(newDescriptionOfProduct);
+                Product.getProductsArrayList().get(i).setQuantityAvailable(newQuantityAvailable);
+                Product.getProductsArrayList().get(i).setPrice(newPrice);
             }
         }
     }
 
-    /// create page to see sales + customer information and revenues from the sale. 
-    
-    
+    public static double calculateRevenue(int quantitySold, double price) {
+        double revenue = quantitySold * price;
+        return revenue;
+    }
+
+    /// create page to see sales + customer information and revenues from the sale.
+
+
 
 
 
@@ -68,4 +94,3 @@ public class Seller extends Product {
 
 
 }
-
