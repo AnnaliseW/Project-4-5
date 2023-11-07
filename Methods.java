86c71b6bb13a24e2b53c8725514cc4ba5ea21591
AnnaliseW <130863140+AnnaliseW@users.noreply.github.com>
2023-11-07 14:02:29 -0500

    Create Methods.java
    made a new class just named methods as some methods are too long so just calling in the main method of marketplace (method includes search for product)

---
+++ Methods.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class Methods {
+
+    // this method searches for a product by what they input
+
+    public ArrayList<Product> searchForProduct(String nameOfProduct, String nameOfStore, String description) {
+        ArrayList<Product> searchedProducts = new ArrayList<>();
+        Product similarProduct;
+        // checking if it matches anything from the product name
+        for (int i = 0; i < SchoolSuppliesMarketplace.getProductsOnMarket().size(); i++) {
+            if (SchoolSuppliesMarketplace.getProductsOnMarket().get(i).getProductName().toLowerCase().contains(nameOfProduct.toLowerCase())) {
+                similarProduct = SchoolSuppliesMarketplace.getProductsOnMarket().get(i);
+                searchedProducts.add(similarProduct);
+
+                // checking if it matches anything from the store name
+            } else if (SchoolSuppliesMarketplace.getProductsOnMarket().get(i).getStoreName().toLowerCase().contains(nameOfStore.toLowerCase())) {
+                similarProduct = SchoolSuppliesMarketplace.getProductsOnMarket().get(i);
+                searchedProducts.add(similarProduct);
+
+                // checking if it matches anything from the description
+            } else if (SchoolSuppliesMarketplace.getProductsOnMarket().get(i).getDescriptionOfProduct().toLowerCase().contains(description.toLowerCase())) {
+                similarProduct = SchoolSuppliesMarketplace.getProductsOnMarket().get(i);
+                searchedProducts.add(similarProduct);
+            }
+        }
+        return searchedProducts;
+    }
+
+
+    public void purchaseProduct(Product product, int quantityPurchased) {
+        for (int i = 0; i < SchoolSuppliesMarketplace.getProductsOnMarket().size(); i++) {
+            if (SchoolSuppliesMarketplace.getProductsOnMarket().get(i).equals(product)) {
+                SchoolSuppliesMarketplace.getProductsOnMarket().get(i).setQuantityAvailable(Product.getQuantityAvailable() - quantityPurchased);
+                SchoolSuppliesMarketplace.getProductsOnMarket().get(i).setQuantitySold(quantityPurchased);
+
+            }
+        }
+    }
+}
