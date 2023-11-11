cecbaeba8f5bb526363a0fb518fe35e8ab3f749e
Joseph <141687242+JHsin23@users.noreply.github.com>
2023-11-11 11:38:48 -0500

    Update Statistics.java

--- Statistics.java
+++ Statistics.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Statistics {
@@ -10,132 +13,179 @@ public class Statistics {
         this.user = user;
     }
 
-    /*
-    Sellers can view a dashboard that lists statistics for each of their stores.
-    Data will include a list of customers with the number of items that they have purchased and a list of products with the number of sales.
-    Sellers can choose to sort the dashboard.
-    */
+    // NO SORTING YET
     public String sellerViewStats() {
-        String message = "";
-        ArrayList<String> Customers1 = new ArrayList<String>();
-        ArrayList<Integer> ItemsPurchased1 = new ArrayList<Integer>();
 
-        ArrayList<Product> Products2 = new ArrayList<Product>();
-        ArrayList<Integer> Sales2 = new ArrayList<Integer>();
+        String result = "";
 
-        if (user.isSeller()) {
+        try (BufferedReader br = new BufferedReader(new FileReader("data.txt"))) {
+            String line;
+            ArrayList<Product> userProducts = null;
 
-            //load in their stores
-            //loop through stores
-            //loop through customers
-            //loop through array and increase count for individual customer purchases
-            //store info in ArrayList 1
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split("@@");
 
-            //loop through stores
-            //loop through products
-            //loop through array and increase count num sales
-            //store info in ArrayList 2
+                String[] userInfo = parts[0].split(",");
+                String email = userInfo[3].trim();
 
-            message += "Customer List:\n";
-            for (int i = 0; i < ItemsPurchased1.size(); i++) {
-                message += Customers1.get(i) + "purchased " + ItemsPurchased1.get(i) + " items." + "\n";
-            }
-            message += "\n";
-            message += "Products List:\n";
-            for (int i = 0; i < Products2.size(); i++) {
-                message += Products2.get(i) + "has " + Products2.get(i) + " sales." + "\n";
-            }
+                if (email.equals(user.getEmail())) {
+                    userProducts = new ArrayList<>();
+                    for (int i = 4; i < parts.length; i++) {
+                        String[] productInfo = parts[i].split(",");
+                        String productName = productInfo[0].trim();
+                        String storeName = productInfo[1].trim();
+                        String descriptionOfProduct = productInfo[2].trim();
+                        int quantityAvailable = Integer.parseInt(productInfo[3].trim());
+                        double price = Double.parseDouble(productInfo[4].trim());
 
-            //finding a way to either call sort method
-            //  or sort inside this and have a parameter for whether they want it sorted or not
-
-
-        } else {
-            message = "Error. You are not a seller. Please log in with a seller account";
-        }
-
-        return message;
-    }
-
-    public String sellerViewStatsSorted() {
-        String message = "";
-        if (user.isSeller()) {
-
-        } else {
-            message = "Error. You are not a seller. Please log in with a seller account";
-        }
-        return message;
-    }
-
-
-    /*
-    Customers can view a dashboard with store and seller information.
-    Data will include a list of stores by number of products sold and a list of stores by the products purchased by that particular customer.
-    Customers can choose to sort the dashboard.
-     */
-    public String customerViewStats() {
-        String message = "";
-        ArrayList<String> Stores1 = new ArrayList<String>();
-        ArrayList<Integer> productsSold1 = new ArrayList<Integer>();
-
-        ArrayList<String> Stores2 = new ArrayList<String>();
-
-        if (!user.isSeller()) {
-
-            //load in stores
-            //loop through them and get number of products sold
-            //put those into Stores1 and productsSold1
-
-            //loop through purchased itmes
-            //if new store, add that store to Store2
-            //go back through purchased items and for each store, get count of items purchased and put that in array
-            String purchased[][] = new String[Stores2.size()][];
-
-            //loop through stores (i)
-            //  int countItems = 0
-            //  loop through all items
-            //      if from that store, countItems++;
-            purchased[i] = new String[countItems];
-            //  go back through all items
-            //      if from store, add it to purchased[i]
-
-            message += "Store List of products sold:\n";
-            for (int i = 0; i < Stores1.size(); i++) {
-                message += Stores1.get(i) + "sold " + productsSold1.get(i) + " items." + "\n";
-            }
-            message += "\n";
-
-            message += "Products List of products purchased:\n";
-            for (int i = 0; i < Stores2.size(); i++) {
-
-                message += "Products purchased from " + Stores2.get(i) + ":";
-                for (int j = 0; j < purchased[i][j].length(); j++) {
-                    message += purchased[i][j] + ",";
-
+                        Product product = new Product(productName, storeName, descriptionOfProduct, quantityAvailable, price);
+                        userProducts.add(product);
+                    }
+                    break;
                 }
-                message = message.substring(0, message.length() - 1);
-                message += "\n";
-
             }
 
-            //finding a way to either call sort method
-            //  or sort inside this and have a parameter for whether they want it sorted or not
+            if (userProducts != null) {
+                result += "User's Products:\n";
+                for (Product product : userProducts) {
+                    result += product + "\n";
+                }
 
-        } else {
-            message = "Error. You are not a customer. Please log in with a customer account";
+                result += "\nSales Statistics:\n";
+                calculateAndAppendSalesStatistics(userProducts, br, result);
+            } else {
+                result += "User not found.";
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        return message;
+        return result;
     }
 
-    public String customerViewStatsSorted() {
-        String message = "";
-        if (!user.isSeller()) {
+    private static void calculateAndAppendSalesStatistics(ArrayList<Product> userProducts, BufferedReader br, String result) throws IOException {
+        String line;
+        while ((line = br.readLine()) != null) {
+            String[] parts = line.split("@@");
 
-        } else {
-            message = "Error. You are not a customer. Please log in with a customer account";
+            for (int i = 4; i < parts.length; i++) {
+                String[] productInfo = parts[i].split(",");
+                String productName = productInfo[0].trim();
+
+                if (userProducts.stream().noneMatch(p -> p.getProductName().equals(productName))) {
+                    int salesCount = calculateSalesCount(productName, parts);
+                    result += "Product name: ";
+                    result += productName;
+                    result += ", ";
+                    result += salesCount;
+                    result += " sales\n";
+                }
+            }
         }
-        return message;
     }
 
+    private static int calculateSalesCount(String productName, String[] parts) {
+        int salesCount = 0;
+        for (int i = 1; i < parts.length; i++) {
+            String[] productInfo = parts[i].split(",");
+            String currentProductName = productInfo[0].trim();
+            if (currentProductName.equals(productName)) {
+                salesCount++;
+            }
+        }
+        return salesCount;
+    }
+
+    // NO SORTING YET
+    public String customerViewStats() {
+        ArrayList<Product> userProducts = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader("data.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split("@@");
+
+                String[] userInfo = parts[0].split(",");
+                String email = userInfo[3].trim();
+
+                if (email.equals(user.getEmail())) {
+                    for (int i = 4; i < parts.length; i++) {
+                        String[] productInfo = parts[i].split(",");
+                        String productName = productInfo[0].trim();
+                        String storeName = productInfo[1].trim();
+                        String descriptionOfProduct = productInfo[2].trim();
+                        int quantityAvailable = Integer.parseInt(productInfo[3].trim());
+                        double price = Double.parseDouble(productInfo[4].trim());
+
+                        Product product = new Product(productName, storeName, descriptionOfProduct, quantityAvailable, price);
+                        userProducts.add(product);
+                    }
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return userProducts.toString();
+    }
+
+    public static ArrayList<String> getStoreNamesForUserProducts(ArrayList<Product> userProducts) {
+        ArrayList<String> storeNames = new ArrayList<>();
+        for (Product product : userProducts) {
+            if (!storeNames.contains(product.getStoreName())) {
+                storeNames.add(product.getStoreName());
+            }
+        }
+        return storeNames;
+    }
+
+    public static ArrayList<ArrayList<String>> getStoresAndProductCount() {
+        ArrayList<ArrayList<String>> storesAndProducts = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader("data.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split("@@");
+
+                for (int i = 4; i < parts.length; i++) {
+                    String[] productInfo = parts[i].split(",");
+                    String storeName = productInfo[1].trim();
+                    boolean isSeller = Boolean.parseBoolean(parts[3].trim());
+
+                    if (isSeller && !containsStore(storesAndProducts, storeName)) {
+                        ArrayList<String> storeAndCount = new ArrayList<>();
+                        storeAndCount.add(storeName);
+                        storeAndCount.add(String.valueOf(getProductCount(parts, storeName)));
+                        storesAndProducts.add(storeAndCount);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return storesAndProducts;
+    }
+
+    private static boolean containsStore(ArrayList<ArrayList<String>> storesAndProducts, String storeName) {
+        for (ArrayList<String> storeAndCount : storesAndProducts) {
+            if (storeAndCount.get(0).equals(storeName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static int getProductCount(String[] parts, String storeName) {
+        int count = 0;
+        for (int i = 4; i < parts.length; i++) {
+            String[] productInfo = parts[i].split(",");
+            if (productInfo[1].trim().equals(storeName)) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
