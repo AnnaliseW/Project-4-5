c86e5d80d898f51c230091870312a148722b5cac
Joseph <141687242+JHsin23@users.noreply.github.com>
2023-11-12 14:53:31 -0500

    Update Files.java
    Updated with better toString call for printing out file

--- Files.java
+++ Files.java
@@ -9,7 +9,7 @@ public class Files {
     public boolean seller;
     public boolean buyer;
 
-    //NEED TO CREATE A FILE OBJECT FIRST
+    //NEED TO CREATE A FILES OBJECT FIRST
     public Files(String infoFileName, boolean seller, boolean buyer) {
         this.infoFileName = infoFileName;
         this.seller = seller;
@@ -22,23 +22,20 @@ public class Files {
 
     public boolean exportPurchaseHistory() throws IOException {
         try (BufferedReader reader = new BufferedReader(new FileReader(infoFileName));
-             BufferedWriter writer = new BufferedWriter(new FileWriter("exportHistory.txt"))) {
+             BufferedWriter writer = new BufferedWriter(new FileWriter("purchaseHistory.txt"))) {
+
             String line;
             while ((line = reader.readLine()) != null) {
-                String breakLine = line;
-                breakLine = breakLine.substring(breakLine.indexOf(",") + 1, breakLine.indexOf(";"));
-                breakLine = breakLine.substring(breakLine.indexOf(",") + 1, breakLine.length());
-                breakLine = breakLine.substring(breakLine.indexOf(",") + 1, breakLine.length());
-                Boolean isSeller = Boolean.parseBoolean(breakLine);
+                String[] fields = line.split(",");
 
-                if (seller == isSeller) { //seller
+                Boolean isSeller = Boolean.parseBoolean(fields[3]);
+
+                if (seller == isSeller) {
                     return false;
-                } else { //buyer
-                    String productsInfo = line;
-                    productsInfo = productsInfo.substring(productsInfo.indexOf("cart"), productsInfo.length() - 1);
-                    String[] products = productsInfo.split(";");
-
-                    for (int i = 0; i < products.length - 1; i++) {
+                } else {
+                    String productsInfo = line.substring(line.indexOf(";") + 1);
+                    String[] products = productsInfo.split("@@");
+                    for (int i = 0; i < products.length; i++) {
                         int index = i + 1;
                         writer.write("Purchase " + index + ": " + products[i]);
                         writer.newLine();
@@ -50,6 +47,7 @@ public class Files {
             e.printStackTrace();
             return false;
         }
+
     }
 
     //Takes in file with each line being a product in this format:
@@ -83,7 +81,7 @@ public class Files {
     public boolean exportProducts(ArrayList<Product> exportedProducts) throws IOException {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("ExportedProducts.txt"))) {
             for (Product product : exportedProducts) {
-                writer.write(product.toString());
+                writer.write(product.statisticsToString());
                 writer.newLine();
             }
         } catch (IOException e) {
@@ -92,5 +90,5 @@ public class Files {
         }
         return true;
     }
-
+    
 }
