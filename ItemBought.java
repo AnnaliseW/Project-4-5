0a0d9c13f0d9e1d15b68e2443db2c4463b1957d6
Joseph <141687242+JHsin23@users.noreply.github.com>
2023-11-12 14:11:21 -0500

    Update ItemBought.java
    updates with the new fields added

--- ItemBought.java
+++ ItemBought.java
@@ -1,14 +1,16 @@
 public class ItemBought {
     private String buyerName;
     private String buyerEmail;
+    private String sellerEmail;
     private String storeName;
     private String productName;
     private double productPrice;
     private int quantityBought;
 
-    public ItemBought(String buyerName, String buyerEmail, String storeName, String productName, double productPrice, int quantityBought) {
+    public ItemBought(String buyerName, String buyerEmail, String sellerEmail, String storeName, String productName, double productPrice, int quantityBought) {
         this.buyerName = buyerName;
         this.buyerEmail = buyerEmail;
+        this.sellerEmail = sellerEmail;
         this.storeName = storeName;
         this.productName = productName;
         this.productPrice = productPrice;
@@ -21,6 +23,10 @@ public class ItemBought {
 
     public String getBuyerEmail() {
         return buyerEmail;
+    }
+
+    public String getSellerEmail() {
+        return sellerEmail;
     }
 
     public String getStoreName() {
@@ -40,14 +46,15 @@ public class ItemBought {
     }
 
     public String toString() {
-        return "ItemBought[" +
+        return "ItemBought{" +
                 "buyerName='" + buyerName + '\'' +
                 ", buyerEmail='" + buyerEmail + '\'' +
+                ", sellerEmail='" + sellerEmail + '\'' +
                 ", storeName='" + storeName + '\'' +
                 ", productName='" + productName + '\'' +
                 ", productPrice=" + productPrice +
                 ", quantityBought=" + quantityBought +
-                ']';
+                '}';
     }
 
 }
