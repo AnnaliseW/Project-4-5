10682058fe29284c9f62d8a9643c502c196fa882
Joseph <141687242+JHsin23@users.noreply.github.com>
2023-11-26 10:27:44 -0500

    Update GUISellerView.java
    Better methods for storing and selection

--- GUISellerView.java
+++ GUISellerView.java
@@ -78,7 +78,63 @@ public class GUISellerView {
 
     }
 
-    public void sellProduct() {
+    public ArrayList<Product> generateMyProducts(User account) {
+        ArrayList<Product> itemsSoldBySeller = new ArrayList<>();
+
+        try {
+            BufferedReader bfr = new BufferedReader(new FileReader("data.txt"));
+            String line = "";
+            ArrayList<String> allUserData = new ArrayList<>();
+
+            while ((line = bfr.readLine()) != null) {
+                allUserData.add(line);
+            }
+
+            for (String userData : allUserData) {
+                String[] oneUserData = userData.split(",");
+                if (oneUserData.length > 1 && oneUserData[1].equals(account.getEmail())) {
+                    String[] separatingForSalesArray = userData.split(";");
+                    if (separatingForSalesArray.length > 1) {
+                        if (!separatingForSalesArray[1].contains("@@")) {
+                            String[] separateParameters = separatingForSalesArray[1].split(",");
+                            //productName, storeName, description, quantityAvailable, price
+                            if (separateParameters.length >= 5) {
+                                String productName = separateParameters[0];
+                                String storeName = separateParameters[1];
+                                String description = separateParameters[2];
+                                int quantityAvailable = Integer.parseInt(separateParameters[3]);
+                                double price = Double.parseDouble(separateParameters[4]);
+                                Product eachProduct = new Product(productName, storeName, description, quantityAvailable, price);
+                                itemsSoldBySeller.add(eachProduct);
+                            }
+                        } else {
+                            String[] arrayListSalesProducts = separatingForSalesArray[1].split("@@");
+                            for (String productData : arrayListSalesProducts) {
+                                String[] separateParameters = productData.split(",");
+                                if (separateParameters.length >= 5) {
+                                    String productName = separateParameters[0];
+                                    String storeName = separateParameters[1];
+                                    String description = separateParameters[2];
+                                    int quantityAvailable = Integer.parseInt(separateParameters[3]);
+                                    double price = Double.parseDouble(separateParameters[4]);
+                                    Product eachProduct = new Product(productName, storeName, description, quantityAvailable, price);
+                                    itemsSoldBySeller.add(eachProduct);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            bfr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return itemsSoldBySeller;
+    }
+
+    public ArrayList<Product> sellProduct(ArrayList<Product> myProducts) {
         JPanel panel = new JPanel(new GridLayout(5, 2));
 
         panel.add(new JLabel("Product Name:"));
@@ -113,15 +169,45 @@ public class GUISellerView {
 
             Product newProductAdded = new Product(productName, storeName, description, quantity, price);
 
-            //TODO: Add products to correct arraylists
+            Methods method = new Methods();
+            method.sellProduct(newProductAdded); //will update products on market (method might be wrong)
+            myProducts.add(newProductAdded);
 
             JOptionPane.showMessageDialog(null, newProductAdded.getProductName() + " added to the market!", "Successfully Added", JOptionPane.INFORMATION_MESSAGE);
 
         }
+        return myProducts;
+
     }
 
 
-    public void editProducts() {
+    //TODO: if they just added the product, it will not appear (even though it should)
+    public ArrayList<Product> editProducts(ArrayList<Product> myProducts) {
+
+        if (myProducts.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "No products to modify.", "No products", JOptionPane.INFORMATION_MESSAGE);
+            return new ArrayList<>();
+        }
+
+        String[] products = new String[myProducts.size()];
+        for (int i = 0; i < myProducts.size(); i++) {
+            products[i] = myProducts.get(i).statisticsToString();
+        }
+        String itemFromSearchChosen = (String) JOptionPane.showInputDialog(null, "Select item to edit",
+                "Edit", JOptionPane.QUESTION_MESSAGE, null, products, products[0]);
+
+        Product productToEdit = myProducts.get(0);
+
+        int selection = 0;
+
+        for (int k = 0; k < myProducts.size(); k++) {
+            if (myProducts.get(k).statisticsToString().equals(itemFromSearchChosen)) {
+                productToEdit = myProducts.get(k);
+                selection = k;
+                break;
+            }
+        }
+
         JPanel panel = new JPanel(new GridLayout(5, 2));
 
         panel.add(new JLabel("New Product Name:"));
@@ -154,15 +240,34 @@ public class GUISellerView {
             int newQuantity = Integer.parseInt(quantityField.getText());
             double newPrice = Double.parseDouble(priceField.getText());
 
+            productToEdit.setProductName(newProductName);
+            productToEdit.setStoreName(newStoreName);
+            productToEdit.setDescriptionOfProduct(newDescription);
+            productToEdit.setQuantityAvailable(newQuantity);
+            productToEdit.setPrice(newPrice);
 
-            Product newProductAdded = new Product(newProductName, newStoreName, newDescription, newQuantity, newPrice);
+            myProducts.get(selection).setProductName(newProductName);
+            myProducts.get(selection).setStoreName(newStoreName);
+            myProducts.get(selection).setDescriptionOfProduct(newDescription);
+            myProducts.get(selection).setQuantityAvailable(newQuantity);
+            myProducts.get(selection).setPrice(newPrice);
 
-            //TODO: Add products to correct arraylists
+            for (int i = 0; i < Methods.getProductsOnMarket().size(); i++) {
+                if (Methods.getProductsOnMarket().get(i).equals(productToEdit)) {
+                    Methods.getProductsOnMarket().get(i).setProductName(newProductName);
+                    Methods.getProductsOnMarket().get(i).setStoreName(newStoreName);
+                    Methods.getProductsOnMarket().get(i).setDescriptionOfProduct(newDescription);
+                    Methods.getProductsOnMarket().get(i).setQuantityAvailable(newQuantity);
+                    Methods.getProductsOnMarket().get(i).setPrice(newPrice);
+                }
+            }
 
-            JOptionPane.showMessageDialog(null, newProductAdded.getProductName() + " added to the market!", "Successfully Added", JOptionPane.INFORMATION_MESSAGE);
+
+            JOptionPane.showMessageDialog(null, "Succesfully modified chosen product!", "Successfully Modified", JOptionPane.INFORMATION_MESSAGE);
 
         }
+        return myProducts;
     }
 
-    
+
 }
