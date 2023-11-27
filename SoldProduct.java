b7acfee1dde8e578394d110238a9921c5be7b5c9
TkamelPurdue <143757582+TkamelPurdue@users.noreply.github.com>
2023-11-27 15:32:44 -0500

    Update SoldProduct.java

--- SoldProduct.java
+++ SoldProduct.java
@@ -1,10 +1,22 @@
 public class SoldProduct extends Product{
+    private static int quantityPurchased;
     private static Customer purchasedBy;
 
     public SoldProduct(String productName, String storeName, String descriptionOfProduct, int quantityAvailable,
-                       double price, Customer purchasedBy){
+                       double price, int quantityPurchased, Customer purchasedBy){
         super(productName, storeName, descriptionOfProduct, quantityAvailable, price);
+        this.quantityPurchased = quantityPurchased;
         this.purchasedBy = purchasedBy;
+    }
+
+    public SoldProduct(Product product, int quantityPurchased){
+        super(product.getProductName(), product.getStoreName(), product.getDescriptionOfProduct(),
+                product.getQuantityAvailable(), product.getPrice());
+        this.quantityPurchased = quantityPurchased;
+    }
+
+    public static int getQuantityPurchased(){
+        return quantityPurchased;
     }
 
     public static Customer getPurchasedBy(){
