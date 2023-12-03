0ea138aa4badcd2a75d63474e04d800dd5f4a2c2
Joseph <141687242+JHsin23@users.noreply.github.com>
2023-12-03 13:59:06 -0500

    Create TestThreadMarketServer.java

---
+++ TestThreadMarketServer.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TestThreadMarketServer {
+
+    private static final int PORT = 1234;
+
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            System.out.println("Server is listening on port " + PORT);
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("New client connected: " + clientSocket);
+
+                Thread clientThread = new Thread(new ClientHandler(clientSocket));
+                clientThread.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class ClientHandler implements Runnable {
+        private final Socket clientSocket;
+
+        public ClientHandler(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+        }
+
+        @Override
+        public void run() {
+            try (
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true)
+            ) {
+                while (true) {
+                    String emailAndPassword = reader.readLine();
+                    if (emailAndPassword == null) {
+                        break;
+                    }
+
+                    //TODO: Rest of code
+
+
+                    writer.println("Server received: " + emailAndPassword);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    clientSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
