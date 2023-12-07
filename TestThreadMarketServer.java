18d80cb35beec6e0a42ecd782eb5907a982f0ea8
Joseph <141687242+JHsin23@users.noreply.github.com>
2023-12-07 15:26:37 -0500

    Update TestThreadMarketServer.java
    New version framework

--- TestThreadMarketServer.java
+++ TestThreadMarketServer.java
@@ -1,60 +1,63 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class TestThreadMarketServer {
 
-    private static final int PORT = 1234;
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(1234);
 
-    public static void main(String[] args) {
-        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
-            System.out.println("Server is listening on port " + PORT);
-
-            while (true) {
-                Socket clientSocket = serverSocket.accept();
-                System.out.println("New client connected: " + clientSocket);
-
-                Thread clientThread = new Thread(new ClientHandler(clientSocket));
-                clientThread.start();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+        while (true) {
+            Socket socket = serverSocket.accept();
+            Thread clientHandlerThread = new Thread(new ClientHandler(socket));
+            clientHandlerThread.start();
         }
     }
 
     private static class ClientHandler implements Runnable {
-        private final Socket clientSocket;
+        private final Socket socket;
 
-        public ClientHandler(Socket clientSocket) {
-            this.clientSocket = clientSocket;
+        public ClientHandler(Socket socket) {
+            this.socket = socket;
         }
 
         @Override
         public void run() {
-            try (
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                    PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true)
-            ) {
-                while (true) {
-                    String emailAndPassword = reader.readLine();
-                    if (emailAndPassword == null) {
-                        break;
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+
+
+                String clientRequest = reader.readLine();
+                if (clientRequest != null) {
+                    if (clientRequest.equals("createAccount")) {
+                        // Call create account method
+                        createAccount();
+                    } else {
+
+
+
                     }
-
-                    //TODO: Rest of code
-
-
-                    writer.println("Server received: " + emailAndPassword);
                 }
+
+                // Continue reading and responding to client requests within this thread
+
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
                 try {
-                    clientSocket.close();
+                    socket.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
+        }
+
+        private static void createAccount() {
+            // Implement create account logic
         }
     }
 }
