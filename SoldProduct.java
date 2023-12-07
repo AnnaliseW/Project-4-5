8d50a8db8dec08b04fad397d384d74f03491d72b
AnnaliseW <130863140+AnnaliseW@users.noreply.github.com>
2023-12-07 18:41:53 -0500

    Update SoldProduct.java
    CHANGING STATISTICS to include quantity bought

--- SoldProduct.java
+++ SoldProduct.java
@@ -1,19 +1,28 @@
-public class SoldProduct extends Product{
-    private static int quantityPurchased;
+public class SoldProduct extends Product {
+    public int quantityPurchased;
 
     public SoldProduct(String productName, String storeName, String descriptionOfProduct, int quantityAvailable,
-                       double price, int quantityPurchased){
+                       double price, int quantityPurchased) {
         super(productName, storeName, descriptionOfProduct, quantityAvailable, price);
         this.quantityPurchased = quantityPurchased;
     }
 
-    public SoldProduct(Product product, int quantityPurchased){
+
+    public SoldProduct(Product product, int quantityPurchased) {
         super(product.getProductName(), product.getStoreName(), product.getDescriptionOfProduct(),
                 product.getQuantityAvailable(), product.getPrice());
         this.quantityPurchased = quantityPurchased;
     }
 
-    public static int getQuantityPurchased(){
-        return quantityPurchased;
+    public int getQuantityPurchased() {
+        return this.quantityPurchased;
+    }
+
+    public String customerRecieptStatistics() {
+        return String.format("Product Name: " +
+                        "%s, Store Name: %s, Description: %s, Quantity Available:" +
+                        " %d, Price: %.2f, Quantity Purchased: %d", productName
+                , storeName, descriptionOfProduct, quantityAvailable, price, quantityPurchased);
+
     }
 }
