919c855cff97fa823e45029182bd5bb0c9a7b42a
Joseph <141687242+JHsin23@users.noreply.github.com>
2023-12-09 20:20:44 -0500

    Update MissingMethods.java

--- MissingMethods.java
+++ MissingMethods.java
@@ -1,174 +1,68 @@
-public static ArrayList<Product> generateMyProducts(User account) {
-        ArrayList<Product> itemsSoldBySeller = new ArrayList<>();
-
-        try {
-            BufferedReader bfr = new BufferedReader(new FileReader("data.txt"));
-            String line = "";
-            ArrayList<String> allUserData = new ArrayList<>();
-
-            while ((line = bfr.readLine()) != null) {
-                allUserData.add(line);
-            }
-
-            for (String userData : allUserData) {
-                String[] oneUserData = userData.split(",");
-                if (oneUserData.length > 1 && oneUserData[1].equals(account.getEmail())) {
-                    String[] separatingForSalesArray = userData.split(";");
-                    if (separatingForSalesArray.length > 1) {
-                        if (!separatingForSalesArray[1].contains("@@")) {
-                            String[] separateParameters = separatingForSalesArray[1].split(",");
-                            //productName, storeName, description, quantityAvailable, price
-                            if (separateParameters.length >= 5) {
-                                String productName = separateParameters[0];
-                                String storeName = separateParameters[1];
-                                String description = separateParameters[2];
-                                int quantityAvailable = Integer.parseInt(separateParameters[3]);
-                                double price = Double.parseDouble(separateParameters[4]);
-                                Product eachProduct = new Product(productName, storeName, description, quantityAvailable, price);
-                                itemsSoldBySeller.add(eachProduct);
-                            }
-                        } else {
-                            String[] arrayListSalesProducts = separatingForSalesArray[1].split("@@");
-                            for (String productData : arrayListSalesProducts) {
-                                String[] separateParameters = productData.split(",");
-                                if (separateParameters.length >= 5) {
-                                    String productName = separateParameters[0];
-                                    String storeName = separateParameters[1];
-                                    String description = separateParameters[2];
-                                    int quantityAvailable = Integer.parseInt(separateParameters[3]);
-                                    double price = Double.parseDouble(separateParameters[4]);
-                                    Product eachProduct = new Product(productName, storeName, description, quantityAvailable, price);
-                                    itemsSoldBySeller.add(eachProduct);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            bfr.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return itemsSoldBySeller;
-    }
+//INSIDE PRODUCT CLASS
+public String statisticsToStringNoSpace() {
+        return String.format("Product Statistics:Product Name: %s Store Name: %s Description: %s Quantity Available: %d Price: %.2f", productName
+                , storeName, descriptionOfProduct, quantityAvailable, price);
+}
 
 
-public void saveDataFileWhenNewProductAddedUserAccount(User userAccount, Product newProduct) {
-        File dataFile = new File("data.txt");
+//INSIDE METHODS
+public void saveDataFileWithNewProductList(User userAccount, ArrayList<Product> newProductList) {
+        File file = new File("data.txt");
+        StringBuilder fileContent = new StringBuilder();
 
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(dataFile));
-
-            ArrayList<String> allLines = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                allLines.add(line);
-            }
-            reader.close();
+                String[] parts = line.split(";");
+                String[] userParts = parts[0].split(",");
 
-            for (int i = 0; i < allLines.size(); i++) {
-                String userData = allLines.get(i);
-                String[] oneUserData = userData.split(",");
-                if (oneUserData.length > 1 && oneUserData[1].equals(userAccount.getEmail())) {
-                    allLines.set(i, userData + "@" + newProduct.toString());
-                    break;
-                }
-            }
+                if (userParts[1].equals(userAccount.getEmail())) {
+                    line = userParts[0] + "," + userParts[1] + "," + userParts[2] + ","
+                            + userParts[3] + ";";
 
-            BufferedWriter writer = new BufferedWriter(new FileWriter(dataFile));
-            for (String updatedLine : allLines) {
-                writer.write(updatedLine);
-                writer.newLine();
-            }
-            writer.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-public void replaceProductInDataFile(Product oldProduct, Product newProduct) {
-        File dataFile = new File("data.txt");
-        ArrayList<String> allUserData = new ArrayList<>();
-
-        try {
-            BufferedReader bfr = new BufferedReader(new FileReader(dataFile));
-            String line;
-            while ((line = bfr.readLine()) != null) {
-                allUserData.add(line);
-            }
-            bfr.close();
-
-            for (int i = 0; i < allUserData.size(); i++) {
-                String[] checkIfSeller = allUserData.get(i).split(",");
-                if (checkIfSeller.length > 3 && checkIfSeller[3].startsWith("true")) {
-                    String[] afterSemiColon = allUserData.get(i).split(";");
-                    if (allUserData.get(i).contains("@@")) {
-                        String[] separatedByProduct = afterSemiColon[1].split("@@");
-                        for (int k = 0; k < separatedByProduct.length; k++) {
-                            String[] findProduct = separatedByProduct[k].split(",");
-                            if (findProduct.length >= 2 && findProduct[1].equals(oldProduct.getProductName())
-                                    && findProduct[0].equals(oldProduct.getStoreName())) {
-                                String updated = newProduct.getStoreName() + "," + newProduct.getProductName() + ","
-                                        + newProduct.getDescriptionOfProduct() + "," + newProduct.getQuantityAvailable()
-                                        + "," + newProduct.getPrice();
-                                allUserData.set(i, allUserData.get(i).replace(separatedByProduct[k], updated));
-                            }
-                        }
+                    for (Product product : newProductList) {
+                        line += product.getProductName() + ","
+                                + product.getStoreName() + ","
+                                + product.getDescriptionOfProduct() + ","
+                                + product.getQuantityAvailable() + ","
+                                + product.getPrice() + "@@";
                     }
                 }
+
+                fileContent.append(line).append("\n");
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        try {
-            PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(dataFile, false)));
-            for (String line : allUserData) {
-                pw.println(line);
-            }
-            pw.flush();
-            pw.close();
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            writer.write(fileContent.toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+//Inside Methods
+public void deleteForProductFile(ArrayList<Product> arrayList, Product itemToDelete) {
+        File dataFile = new File("productArrayList.txt");
 
-public void removeProductFromDataFile(User userAccount, Product productToRemove) {
-        File dataFile = new File("data.txt");
+        ArrayList<String> allProducts = new ArrayList<>();
 
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(dataFile));
-
-            ArrayList<String> allLines = new ArrayList<>();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                allLines.add(line);
+        for (Product product : arrayList) {
+            if (!product.equals(itemToDelete)) {
+                String productString = product.getProductName() + "," +
+                        product.getStoreName() + "," +
+                        product.getDescriptionOfProduct() + "," +
+                        product.getQuantityAvailable() + "," +
+                        product.getPrice() + "@@";
+                allProducts.add(productString);
             }
-            reader.close();
+        }
 
-            for (int i = 0; i < allLines.size(); i++) {
-                String userData = allLines.get(i);
-                String[] oneUserData = userData.split(",");
-                if (oneUserData.length > 1 && oneUserData[1].equals(userAccount.getEmail())) {
-                    String productStringToRemove = productToRemove.toString();
-                    if (userData.contains(productStringToRemove)) {
-                        String updatedUserData = userData.replace("@" + productStringToRemove, "");
-                        allLines.set(i, updatedUserData);
-                        break;
-                    }
-                }
+        try (PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(dataFile, false)))) {
+            for (String productString : allProducts) {
+                pw.print(productString);
             }
-
-            BufferedWriter writer = new BufferedWriter(new FileWriter(dataFile));
-            for (String updatedLine : allLines) {
-                writer.write(updatedLine);
-                writer.newLine();
-            }
-            writer.close();
-
         } catch (IOException e) {
             e.printStackTrace();
         }
